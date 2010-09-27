e4971eb564ee21bc4ec27543ea70455e3aeb6965
Victor Vicente de Carvalho <victor.v.carvalho@gmail.com>
2010-09-26 21:32:09 -0300

    furter improvements on testing

--- test/simple_server_tester.py
+++ test/simple_server_tester.py
@@ -14,10 +14,15 @@ import asyncore
 from common.json_handler import Handler
 import logging
 import time
+import threading
 
 logging.basicConfig(level=logging.INFO, format= '%(asctime)s %(levelname)-8s %(module)-20s[%(lineno)-3d] %(message)s')
 
 class Client(ClientHandler):
+    LOBBY = 0
+    GAME_LOBBY = 1
+    IN_GAME = 2
+    
     def __init__(self, ip, port, sock = None):
         ClientHandler.__init__(self)
 
@@ -27,33 +32,91 @@ class Client(ClientHandler):
                                  'ping' : self.handle_ping, 
                                  'lobby_session_logon' : self.session_logon,
                                  'lobby_info' : self.handle_lobby_info,
+                                 'logon_response' : self.handle_session_logon,
                                  'lobby_create_game' : self.handle_game_create }
 
+        lobby_actions = [ ('say something' , { 'action' : 'lobby_chat', 'fields' : [ ('message', str, True) ] }),
+                          ('create game' , { 'action' : 'lobby_create_game', 'fields' : [('game_type', str, True), ('room_name', str, True) ] } ) ]
+        
+        game_lobby_actions  = [ ('say something' , { 'action' : 'lobby_chat', 'fields' : [ ('message', str, True) ] }),
+                                ('change color', { 'action' : 'lobby_set_self_color', 'fields' : [ ('color', str, True) ] } ) ]
+        
+        self.menu = [ lobby_actions, game_lobby_actions ]
+        
+        self.current_state = self.LOBBY
+        
         self.read_handler = self.handle_message
 
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((ip , port))
         
+        self.showing_menu = False
+                
+    def print_menu(self):
+      def respawn():
+        threading.Timer(1, self.print_menu).start()
+      
+      if self.showing_menu:
+        return
+      
+      self.showing_menu = True
+      
+      i = 0
+      for item in self.menu[self.current_state]:
+        print "{0} - {1}".format(i, item[0])
+        i += 1
+      
+      value = raw_input("select a value: ")
+      params = {}
+      
+      try:
+        value = int(value)
+      except:
+        print 'Error: invalid format for menu value'
+        respawn()
+        return
+      
+      if value > len(self.menu[self.current_state]):
+        print ' invalid value {0}.'.format(value)
+        respawn()
+        return
+      
+      reqd = { True : ' (required) ', False : ' (enter to proceed) ' }
+      for param in self.menu[self.current_state][value][1]['fields']:
+        item_value = raw_input('{0}{1}: '.format(param[0], reqd[param[2]]))        
+        params[param[0]] = param[1](item_value)
+      
+      message = params
+      message['action'] = self.menu[self.current_state][value][1]['action']
+      
+      self.write(message)
+      
+      self.showing_menu = False
+      
     def session_logon(self, message):
       print "session {0} logged.".format(message['username'])
     
     def handle_lobby_info(self, message):
-      print 'received lobby info. Creating game'
-      self.write({ 'action' : 'lobby_create_game', 'game_type' : 'wargame', 'room_name' : 'cocadinha' })
-    
+      print 'received lobby info. :', message
+      
     def handle_ping(self, message):
         if 'time' in message:
             before = float(message['time'])
             print 'response delay:', time.time() - before
 
     def handle_game_create(self, message):
-      print "Created game with id {0}".format(message['game_id'])
+      if message['status'] == 'sucessfull':
+        self.current_state = self.GAME_LOBBY
+      else:
+        print 'error creating game: ', message
       
     def handle_session_logon(self, message):
-        if message['username'] == 'user':
-            self.write({"action" : "lobby_chat" , 'message' : "oi mamae"})
+        print 'received seession logon response: ', message
+        if message['authenticated'] == 'yes':
+            print 'authenticated'
             
     def handle_connection_response(self, message): 
+        print 'received connection response: ', message
         self.write({"action" : "login", "username" : "user", "password" : "123456"})
     
     def write(self, message):
@@ -68,7 +131,10 @@ class Client(ClientHandler):
         if message['action'] in self.action_handlers:            
             self.action_handlers[message['action']](message)
         else:
-            logging.info("Unhandled message: {0}".format(message))
+            print "Unhandled message: {0}".format(message)
+        
+        if not self.showing_menu:
+          threading.Timer(1, self.print_menu).start()
         
 if __name__ == "__main__":
     clients = []
