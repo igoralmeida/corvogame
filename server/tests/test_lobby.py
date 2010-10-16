b8a06eb412ff716f76d75905ade4a899dded593c
Victor Vicente de Carvalho <victor.v.carvalho@gmail.com>
2010-10-16 14:17:31 -0300

    alterando a parte de testes também

--- server/tests/test_lobby.py
+++ server/tests/test_lobby.py
@@ -21,8 +21,8 @@ def test_setting_color():
   assert(session1['self_color'] not in l.available_colors)
   
   l.handle_set_self_color(session1, { 'action' : 'lobby_set_self_color', 'color' : 'red'} )
-  session1.expect('game_lobby_player_updated_color')
-  assert('red' not in session1.expect('game_lobby_available_colors')['colors'])
+  session1.expect('wargame_lobby_player_updated_color')
+  assert('red' not in session1.expect('wargame_lobby_available_colors')['colors'])
   
   l.stop()
   l.join()
@@ -39,15 +39,15 @@ def test_setting_same_color():
   
   assert(session1['self_color'] not in l.available_colors)
   
-  l.handle_set_self_color(session1, { 'action' : 'game_lobby_set_self_color', 'color' : 'red'} )
-  session1.expect('game_lobby_player_updated_color')
+  l.handle_set_self_color(session1, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'red'} )
+  session1.expect('wargame_lobby_player_updated_color')
   
-  assert('red' not in session1.expect('game_lobby_available_colors')['colors'])
+  assert('red' not in session1.expect('wargame_lobby_available_colors')['colors'])
   
   session1.purge()  
-  l.handle_set_self_color(session1, { 'action' : 'game_lobby_set_self_color', 'color' : 'red'} )
+  l.handle_set_self_color(session1, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'red'} )
   
-  assert(session1.expect('game_lobby_set_self_color')['status'] == 'error')
+  assert(session1.expect('wargame_lobby_set_self_color')['status'] == 'error')
   
   l.stop()
   l.join()
@@ -65,13 +65,13 @@ def test_setting_color_users():
   assert(session1['self_color'] not in l.available_colors)
   assert(session2['self_color'] not in l.available_colors)
   
-  l.handle_set_self_color(session1, { 'action' : 'game_lobby_set_self_color', 'color' : 'red'} )
-  print session2.expect('game_lobby_player_updated_color')
-  assert(session2.expect('game_lobby_player_updated_color')['username'] == session1.username)
+  l.handle_set_self_color(session1, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'red'} )
+  print session2.expect('wargame_lobby_player_updated_color')
+  assert(session2.expect('wargame_lobby_player_updated_color')['username'] == session1.username)
   
-  l.handle_set_self_color(session2, { 'action' : 'game_lobby_set_self_color', 'color' : 'blue'} )
-  print session1.expect('game_lobby_player_updated_color')
-  assert(session1.expect('game_lobby_player_updated_color')['username'] == session2.username)
+  l.handle_set_self_color(session2, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'blue'} )
+  print session1.expect('wargame_lobby_player_updated_color')
+  assert(session1.expect('wargame_lobby_player_updated_color')['username'] == session2.username)
   
   l.stop()
   l.join()
@@ -83,11 +83,11 @@ def test_change_color():
   session1 = MockSession('user1')
   l.add_session(session1, {})
   
-  l.handle_set_self_color(session1, { 'action' : 'game_lobby_set_self_color', 'color' : 'red'} )
-  session1.expect('game_lobby_player_updated_color')
+  l.handle_set_self_color(session1, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'red'} )
+  session1.expect('wargame_lobby_player_updated_color')
   session1.purge()
   
-  l.handle_set_self_color(session1, { 'action' : 'game_lobby_set_self_color', 'color' : 'red'} )
+  l.handle_set_self_color(session1, { 'action' : 'wargame_lobby_set_self_color', 'color' : 'red'} )
   
   print session1
   
@@ -112,16 +112,16 @@ def test_ready_game():
   l.handle_set_self_ready(session2, { 'ready' : 'true' } )
   l.handle_set_self_ready(session3, { 'ready' : 'true' } )
   
-  session1.expect('game_lobby_player_ready_state')
-  session2.expect('game_lobby_player_ready_state')
-  session3.expect('game_lobby_player_ready_state')
+  session1.expect('wargame_lobby_player_ready_state')
+  session2.expect('wargame_lobby_player_ready_state')
+  session3.expect('wargame_lobby_player_ready_state')
   
   l.handle_start_game(session1, { } )  
   
   print "waiting for session1 lobby start game rejection due not having ownership"
   
-  assert( session1.expect( 'game_lobby_start_game' )['status'] == 'error' )
-  assert( session1.expect( 'game_lobby_start_game' )['reason'] == 'you are not the room owner' )
+  assert( session1.expect( 'wargame_lobby_start_game' )['status'] == 'error' )
+  assert( session1.expect( 'wargame_lobby_start_game' )['reason'] == 'you are not the room owner' )
   
   print "done"
   
@@ -129,13 +129,13 @@ def test_ready_game():
   
   print "waiting for room owner lobby start game rejection due not all players being ready"
   
-  assert( room_owner.expect( 'game_lobby_start_game' )['status'] == 'error' )
-  assert( room_owner.expect( 'game_lobby_start_game' )['reason'] == 'there are players not ready to play' )
+  assert( room_owner.expect( 'wargame_lobby_start_game' )['status'] == 'error' )
+  assert( room_owner.expect( 'wargame_lobby_start_game' )['reason'] == 'there are players not ready to play' )
  
   print "done"
   
   l.handle_set_self_ready(session4, { 'ready' : 'true' } )
-  l.handle_start_game(room_owner, { 'action' : 'game_lobby_start_game' } )
+  l.handle_start_game(room_owner, { 'action' : 'wargame_lobby_start_game' } )
   
   l.stop()
   l.join()
