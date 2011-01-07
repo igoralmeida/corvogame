a7d4b2589c9caca46ff059093ef605b35c0b3be5
Igor Almeida <igor.contato@gmail.com>
2011-01-06 23:19:56 -0300

    Wargame Client: handler do handshake

--- client/games/wargame_client.py
+++ client/games/wargame_client.py
@@ -19,10 +19,30 @@ import abstract_game
 import logging
 
 class WargameHandler(abstract_game.GameHandler):
+    available_colors = []
+    available_capabilities = []
+
     def __init__(self):
         abstract_game.GameHandler.__init__(self)
         self.read_handler = self.lobby_handler
 
     def lobby_handler(self, msg):
         logging.debug('Wargame handling message {0}'.format(msg))
+
+        action = msg[u'action'][14:] #remove 'wargame_lobby_' from msg
+
+        if action == 'game_capabilities':
+            colors = msg[u'available_colors']
+            self.update_colors(colors)
+
+            caps = msg[u'capabilities']
+            self.update_capabilities(caps)
+        elif action == 'join':
+            pass
+
+    def update_colors(self, colors):
+        self.available_colors = colors
+
+    def update_capabilities(self, caps):
+        self.available_capabilities = caps
 
